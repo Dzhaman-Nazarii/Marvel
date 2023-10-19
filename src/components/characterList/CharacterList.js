6c65391d3375e69c155d4a2ead8170141d072998
Dzhaman-Nazarii <dzhaman.nazar2003@gmail.com>
2023-10-19 18:07:51 +0300

    update CharacterList

--- src/components/characterList/CharacterList.js
+++ src/components/characterList/CharacterList.js
@@ -2,10 +2,14 @@ import { Component } from "react";
 import "./CharacterList.scss";
 
 import MarvelService from "../../services/MarvelService";
+import ErrorMessage from "../errorMessage/ErrorMessage";
+import Spinner from "../spinner/Spinner";
 
 class CharacterList extends Component {
   state = {
     characters: [],
+    loading: true,
+    error: false,
   };
 
   marvelService = new MarvelService();
@@ -16,28 +20,48 @@ class CharacterList extends Component {
 
   onLoadedList = () => {
     this.marvelService.getCharacterList().then((data) => {
-      this.setState({ characters: data });
+      this.setState({ characters: data, loading: false });
+    });
+  };
+
+  onCharacterListLoaded = (characters) => {
+    this.setState({
+      characters,
+      loading: false,
+    });
+  };
+
+  onError = () => {
+    this.setState({
+      error: true,
+      loading: false,
     });
   };
 
   render() {
+    const { characters, loading, error } = this.state;
+
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const characterItems = characters.map(({ id, name, thumbnail }) => (
+      <li
+        key={id}
+        onClick={() => this.props.onCharacterSelected(id)}
+        className="character__item"
+      >
+        <img src={thumbnail} alt={name} />
+        <div className="character__name">{name}</div>
+      </li>
+    ));
+
     return (
       <div className="character__list">
-        <ul className="character__grid">
-          {this.state.characters.map(({ id, name, thumbnail }) => (
-            <li
-              key={id}
-              onClick={() => this.props.onCharacterSelected(id)}
-              className="character__item"
-            >
-              <img src={thumbnail} alt={name} />
-              <div className="character__name">{name}</div>
-            </li>
-          ))}
-        </ul>
+        <ul className="character__grid">{characterItems}</ul>
         <button className="button button__main button__long">
-          <div className="inner">load more</div>
+          <div className="inner">Завантажити ще</div>
         </button>
+        {errorMessage}
+        {spinner}
       </div>
     );
   }
